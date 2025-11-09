08c2dc57d196019fa6e84d177c68a00c4b9ca3ad
GSpy <55821668+G5py@users.noreply.github.com>
2025-11-10 08:36:08 +0900

    feat : 기상청 API URL 추가.

--- src/main/java/com/github/yun531/climate/weatherApi/WeatherApiUrls.java
+++ src/main/java/com/github/yun531/climate/weatherApi/WeatherApiUrls.java
@@ -3,8 +3,10 @@ package com.github.yun531.climate.weatherApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("weatherapi.short.grid")
+@ConfigurationProperties("weatherapi")
 @RequiredArgsConstructor
 public class WeatherApiUrls {
     public final String SHORT_GRID_FORECAST;
+    public final String MID_TEMPERATURE_FORECAST;
+    public final String MID_LAND_FORECAST;
 }
