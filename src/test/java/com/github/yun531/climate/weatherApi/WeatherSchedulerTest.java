f675c9c62b328a7665b990153cea2ae2853ec79d
GSpy <55821668+G5py@users.noreply.github.com>
2025-12-13 14:13:14 +0900

    test : 단기 격자 예보를 db에 저장하는 테스트 코드 추가.

---
+++ src/test/java/com/github/yun531/climate/weatherApi/WeatherSchedulerTest.java
@@ -0,0 +1,25 @@
+/*
+   단기 격자 예보 데이터를 불러오는 테스트 코드
+   한 번 실행 시 2~3 분 정도 걸림.
+   아래 코드의 주석 처리를 지우고 실행할 것.
+*/
+
+//package com.github.yun531.climate.weatherApi;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//class WeatherSchedulerTest {
+//
+//    @Autowired
+//    private WeatherScheduler scheduler;
+//
+//    @Test
+//    void doShortTermGrid() {
+//        scheduler.doShortTermGrid();
+//    }
+//}
