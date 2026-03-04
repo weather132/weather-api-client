78a9ae3440268d4a792ef68be93cea7222ddc908
GSpy <55821668+G5py@users.noreply.github.com>
2026-03-04 17:11:03 +0900

    test : MidLandRepositoryImpl 테스트 코드 추가.

---
+++ src/test/java/com/github/yun531/climate/midLand/infra/MidLandRepositoryImplTest.java
@@ -0,0 +1,65 @@
+package com.github.yun531.climate.midLand.infra;
+
+import com.github.yun531.climate.common.MidAnnounceTime;
+import com.github.yun531.climate.midLand.domain.MidLand;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+@Import(MidLandRepositoryImpl.class)
+class MidLandRepositoryImplTest {
+    @Autowired
+    private MidLandRepositoryImpl midLandRepositoryImpl;
+
+    @Container
+    static MySQLContainer<?> mysql =
+            new MySQLContainer<>("mysql:8.0")
+                    .withDatabaseName("climate")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    @DynamicPropertySource
+    static void datasourceConfig(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+    }
+
+    @Test
+    void saveAll_및_조회() {
+        // given
+        MidAnnounceTime announceTime = new MidAnnounceTime(LocalDateTime.of(2026, 3, 1, 12, 0, 0));
+        LocalDateTime effectiveTime = announceTime.getTime().plusHours(1);
+
+        List<MidLand> midLands = new ArrayList<>();
+        midLands.add(new MidLand(announceTime, effectiveTime, 1L, 1));
+        midLands.add(new MidLand(announceTime, effectiveTime, 2L, 2));
+
+        // when
+        midLandRepositoryImpl.saveAll(midLands);
+
+        // then
+        List<MidLand> actual = midLandRepositoryImpl.findAll();
+        assertEquals(1, actual.get(0).getPop());
+        assertEquals(2, actual.get(1).getPop());
+    }
+}
---
+++ src/test/resources/schema.sql
@@ -0,0 +1,74 @@
+create database if not exists climate;
+use climate;
+
+create table province_region_code
+(
+    id bigint primary key auto_increment,
+    region_code varchar(20) not null unique
+);
+
+create table city_region_code(
+    id bigint primary key auto_increment,
+    region_code varchar(20) not null unique,
+    x int not null,
+    y int not null,
+    province_region_code_id bigint,
+    foreign key (province_region_code_id) references province_region_code(id) on delete set null
+);
+
+create table short_grid(
+    id bigint primary key auto_increment,
+    announce_time datetime not null,
+    effective_time datetime not null,
+    x int not null,
+    y int not null,
+    pop int,
+    temp int,
+    unique key unique_set(announce_time, effective_time, x, y)
+);
+
+create table short_land(
+    id bigint primary key auto_increment,
+    announce_time datetime not null,
+    effective_time datetime not null,
+    city_region_code_id bigint not null,
+    pop int,
+    temp int,
+    rain_type int,
+    foreign key (city_region_code_id) references city_region_code(id) on delete cascade
+);
+
+create table mid_pop(
+	id bigint primary key auto_increment,
+	announce_time datetime not null,
+    effective_time datetime not null,
+    province_region_code_id bigint not null,
+    pop int not null,
+    foreign key (province_region_code_id) references province_region_code(id) on delete cascade
+);
+
+create table mid_temperature(
+	id bigint primary key auto_increment,
+    announce_time datetime not null,
+    effective_time datetime not null,
+    city_region_code_id bigint not null,
+	max_temp int,
+    min_temp int,
+    foreign key (city_region_code_id) references city_region_code(id) on delete cascade
+);
+
+
+insert into province_region_code values(null, "11B00000");
+insert into province_region_code values(null, "11D10000");
+insert into province_region_code values(null, "11D20000");
+insert into province_region_code values(null, "11C20000");
+insert into province_region_code values(null, "11C10000");
+
+insert into city_region_code values(null, "11B10101", 60, 127, null);
+insert into city_region_code values(null, "11B20201", 55, 124, null);
+insert into city_region_code values(null, "11B20601", 60, 121, null);
+insert into city_region_code values(null, "11B20605", 63, 124, null);
+insert into city_region_code values(null, "11B20602", 59, 123, null);
+insert into city_region_code values(null, "11B10103", 58, 125, null);
+insert into city_region_code values(null, "11B10102", 60, 124, null);
+insert into city_region_code values(null, "11B20606", 62, 114, null);
