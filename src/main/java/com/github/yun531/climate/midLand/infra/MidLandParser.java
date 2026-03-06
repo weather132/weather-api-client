5d3e5869bc1b7c93d28637ed28115145aff637dd
GSpy <55821668+G5py@users.noreply.github.com>
2026-03-06 09:18:13 +0900

    feat : 예외처리 구문 추가.

--- src/main/java/com/github/yun531/climate/midLand/infra/MidLandParser.java
+++ src/main/java/com/github/yun531/climate/midLand/infra/MidLandParser.java
@@ -5,8 +5,10 @@ import com.github.yun531.climate.common.parseConfig.ParseConfig;
 import com.github.yun531.climate.midLand.domain.MidLand;
 import com.github.yun531.climate.provinceRegionCode.ProvinceRegionCode;
 import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -18,6 +20,15 @@ public class MidLandParser {
     }
 
     public List<MidLand> parse(String raw, MidAnnounceTime announceTime, ProvinceRegionCode regionCode) {
+        try {
+            return _parse(raw, announceTime, regionCode);
+
+        } catch (PathNotFoundException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    private List<MidLand> _parse(String raw, MidAnnounceTime announceTime, ProvinceRegionCode regionCode) {
         MidLandItem item = JsonPath.using(parseConfig.getConfiguration()).parse(raw).read("$.response.body.items.item[0]", MidLandItem.class);
         return item.toMidLands(announceTime, regionCode.getId());
     }
