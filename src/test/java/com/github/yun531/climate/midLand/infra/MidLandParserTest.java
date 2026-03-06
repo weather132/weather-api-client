bc43475a9b78c34bfa3eeace678c2ec8c08435d5
GSpy <55821668+G5py@users.noreply.github.com>
2026-03-06 09:19:29 +0900

    test : MidLandParser가 비정상 JSON을 파싱하는 경우의 테스트 케이스 추가.

--- src/test/java/com/github/yun531/climate/midLand/infra/MidLandParserTest.java
+++ src/test/java/com/github/yun531/climate/midLand/infra/MidLandParserTest.java
@@ -29,7 +29,7 @@ class MidLandParserTest {
     ProvinceRegionCode mockRegionCode;
 
     @Test
-    void 정상적인_JSON파일_파싱() throws IOException {
+    void 정상_JSON_파싱() throws IOException {
         // given
         ClassPathResource resource = new ClassPathResource("mid-land-testcase.json");
         String rawJson = new String(resource.getInputStream().readAllBytes());
@@ -42,5 +42,20 @@ class MidLandParserTest {
 
         // then
         assertThat(actual).hasSize(9);
+    }
+
+    @Test
+    void 비정상_JSON_파싱() {
+        // given
+        String rawJson = "{}";
+        when(mockRegionCode.getId()).thenReturn(1L);
+
+        // when
+        List<MidLand> actual = parser.parse(rawJson,
+                new MidAnnounceTime(LocalDateTime.of(2026, 3, 5, 12, 0)),
+                mockRegionCode);
+
+        // then
+        assertThat(actual).hasSize(0);
     }
 }
