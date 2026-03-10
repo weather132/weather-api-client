f16d13aab3b18508e6c91d46c6b2890619d41e91
GSpy <55821668+G5py@users.noreply.github.com>
2026-03-10 10:46:22 +0900

    feat : 예외처리 구문 추가.

--- src/main/java/com/github/yun531/climate/midTemperature/infra/MidTemperatureParser.java
+++ src/main/java/com/github/yun531/climate/midTemperature/infra/MidTemperatureParser.java
@@ -1,31 +1,40 @@
 package com.github.yun531.climate.midTemperature.infra;
 
+import com.github.yun531.climate.cityRegionCode.domain.CityRegionCode;
 import com.github.yun531.climate.cityRegionCode.domain.CityRegionCodeRepository;
 import com.github.yun531.climate.common.MidAnnounceTime;
 import com.github.yun531.climate.common.parseConfig.ParseConfig;
 import com.github.yun531.climate.midTemperature.domain.MidTemperature;
 import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class MidTemperatureParser {
     private final ParseConfig parseConfig;
-    private final CityRegionCodeRepository cityRegionCodeRepository;
 
-    public MidTemperatureParser(ParseConfig parseConfig, CityRegionCodeRepository cityRegionCodeRepository) {
+    public MidTemperatureParser(ParseConfig parseConfig) {
         this.parseConfig = parseConfig;
-        this.cityRegionCodeRepository = cityRegionCodeRepository;
     }
 
-    public List<MidTemperature> parse(String raw, MidAnnounceTime announceTime) {
+    public List<MidTemperature> parse(String raw, MidAnnounceTime announceTime, CityRegionCode regionCode) {
+        try {
+            return _parse(raw, announceTime, regionCode);
+
+        } catch (PathNotFoundException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    private List<MidTemperature> _parse(String raw, MidAnnounceTime announceTime, CityRegionCode regionCode) {
         MidTempItem item = JsonPath
                 .using(parseConfig.getConfiguration())
                 .parse(raw)
                 .read("$.response.body.items.item[0]", MidTempItem.class);
 
-        return item.toMidTemperatures(announceTime, cityRegionCodeRepository.findByRegionCode((item.getRegId())));
-
+        return item.toMidTemperatures(announceTime, regionCode.getId());
     }
 }
