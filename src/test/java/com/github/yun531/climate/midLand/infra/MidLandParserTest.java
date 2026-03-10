46064df7ff3b1469dffcdb9326d27411df8def35
GSpy <55821668+G5py@users.noreply.github.com>
2026-03-10 10:08:00 +0900

    test : 테스트 코드 수정.

--- src/test/java/com/github/yun531/climate/midLand/infra/MidLandParserTest.java
+++ src/test/java/com/github/yun531/climate/midLand/infra/MidLandParserTest.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 
