ea6ee08689c77e5b0007448674156b871e264ddb
GSpy <55821668+G5py@users.noreply.github.com>
2026-03-10 10:05:59 +0900

    test : 외부 파일로 저장되어 있던 테스트 케이스를 테스트 코드에 하드코딩되도록 수정.

--- src/test/java/com/github/yun531/climate/midLand/infra/MidLandParserTest.java
+++ src/test/java/com/github/yun531/climate/midLand/infra/MidLandParserTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -31,11 +30,8 @@ class MidLandParserTest {
     @Test
     void 정상_JSON_파싱() throws IOException {
         // given
-        ClassPathResource resource = new ClassPathResource("mid-land-testcase.json");
-        String rawJson = new String(resource.getInputStream().readAllBytes());
-
+        String rawJson = getJsonTestCase();
         when(mockRegionCode.getId()).thenReturn(1L);
-
         MidAnnounceTime announceTime = new MidAnnounceTime(LocalDateTime.of(2026, 3, 5, 12, 0));
 
         // when
@@ -61,5 +57,52 @@ class MidLandParserTest {
 
         // then
         assertThat(actual).hasSize(0);
+    }
+
+
+    String getJsonTestCase() {
+        return "{\n" +
+                "  \"response\": {\n" +
+                "    \"header\": {\n" +
+                "      \"resultCode\": \"00\",\n" +
+                "      \"resultMsg\": \"NORMAL_SERVICE\"\n" +
+                "    },\n" +
+                "    \"body\": {\n" +
+                "      \"dataType\": \"JSON\",\n" +
+                "      \"items\": {\n" +
+                "        \"item\": [\n" +
+                "          {\n" +
+                "            \"regId\": \"11B00000\",\n" +
+                "            \"rnSt4Am\": 10,\n" +
+                "            \"rnSt4Pm\": 20,\n" +
+                "            \"rnSt5Am\": 10,\n" +
+                "            \"rnSt5Pm\": 10,\n" +
+                "            \"rnSt6Am\": 10,\n" +
+                "            \"rnSt6Pm\": 10,\n" +
+                "            \"rnSt7Am\": 10,\n" +
+                "            \"rnSt7Pm\": 20,\n" +
+                "            \"rnSt8\": 30,\n" +
+                "            \"rnSt9\": 30,\n" +
+                "            \"rnSt10\": 30,\n" +
+                "            \"wf4Am\": \"맑음\",\n" +
+                "            \"wf4Pm\": \"맑음\",\n" +
+                "            \"wf5Am\": \"맑음\",\n" +
+                "            \"wf5Pm\": \"맑음\",\n" +
+                "            \"wf6Am\": \"맑음\",\n" +
+                "            \"wf6Pm\": \"맑음\",\n" +
+                "            \"wf7Am\": \"맑음\",\n" +
+                "            \"wf7Pm\": \"맑음\",\n" +
+                "            \"wf8\": \"구름많음\",\n" +
+                "            \"wf9\": \"구름많음\",\n" +
+                "            \"wf10\": \"구름많음\"\n" +
+                "          }\n" +
+                "        ]\n" +
+                "      },\n" +
+                "      \"pageNo\": 1,\n" +
+                "      \"numOfRows\": 1,\n" +
+                "      \"totalCount\": 1\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
     }
 }
