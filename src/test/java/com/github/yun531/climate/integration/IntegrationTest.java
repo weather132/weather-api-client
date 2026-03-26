0d3196707dd76fc25df9d865c3fd78821d5f366f
GSpy <55821668+G5py@users.noreply.github.com>
2026-03-26 16:17:57 +0900

    test : 통합 테스트 파일 추가.

---
+++ src/test/java/com/github/yun531/climate/integration/IntegrationTest.java
@@ -0,0 +1,12 @@
+package com.github.yun531.climate.integration;
+
+import org.junit.Before;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class IntegrationTest {
+    @Before
+    public void init() {
+
+    }
+}
