11252d18ff00cadfb3cabf2bef61bfd23aef1568
GSpy <55821668+G5py@users.noreply.github.com>
2026-03-26 16:23:31 +0900

    test : mysql을 테스트 db로 사용하기 위한 코드 추가.

--- src/test/java/com/github/yun531/climate/integration/IntegrationTest.java
+++ src/test/java/com/github/yun531/climate/integration/IntegrationTest.java
@@ -1,10 +1,33 @@
 package com.github.yun531.climate.integration;
 
 import org.junit.Before;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class IntegrationTest {
+
+    @Container
+    static MySQLContainer<?> mysql =
+            new MySQLContainer<>("mysql:8.0")
+                    .withDatabaseName("climate")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    @DynamicPropertySource
+    static void datasourceConfig(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+    }
+
     @Before
     public void init() {
 
