0525b9dc95b06867fa97fe26b43cf5e2c5214d5e
GSpy <55821668+G5py@users.noreply.github.com>
2026-04-07 16:17:36 +0900

    test : 단기 격자 좌표들이 고유한지 검사하는 테스트 코드 추가.

--- src/test/java/com/github/yun531/climate/shortGrid/infra/ShortGridClientImplTest.java
+++ src/test/java/com/github/yun531/climate/shortGrid/infra/ShortGridClientImplTest.java
@@ -1,11 +1,15 @@
 package com.github.yun531.climate.shortGrid.infra;
 
+import com.github.yun531.climate.cityRegionCode.domain.CityRegionCode;
+import com.github.yun531.climate.cityRegionCode.domain.CityRegionCodeRepository;
+import com.github.yun531.climate.cityRegionCode.domain.Coordinates;
 import com.github.yun531.climate.common.apiKey.ApiKey;
 import com.github.yun531.climate.shortGrid.domain.AnnounceTime;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
@@ -48,5 +52,43 @@ class ShortGridClientImplTest {
 
         // then
         assertThat(actual).isEqualTo("2026031212");
+    }
+
+    @Test
+    void getCoords_중복된_좌표_distinct_정상() {
+        // given
+        Coordinates coords1 = new Coordinates();
+        ReflectionTestUtils.setField(coords1, "x", 1);
+        ReflectionTestUtils.setField(coords1, "y", 1);
+
+        Coordinates coords2 = new Coordinates();
+        ReflectionTestUtils.setField(coords2, "x", 2);
+        ReflectionTestUtils.setField(coords2, "y", 2);
+
+        Coordinates coords3 = new Coordinates();
+        ReflectionTestUtils.setField(coords3, "x", 2);
+        ReflectionTestUtils.setField(coords3, "y", 2);
+
+        CityRegionCode city1 = mock(CityRegionCode.class);
+        when(city1.getCoordinates()).thenReturn(coords1);
+
+        CityRegionCode city2 = mock(CityRegionCode.class);
+        when(city2.getCoordinates()).thenReturn(coords2);
+
+        CityRegionCode city3 = mock(CityRegionCode.class);
+        when(city3.getCoordinates()).thenReturn(coords3);
+
+        List<CityRegionCode> cityRegions = List.of(city1, city2, city3);
+
+        CityRegionCodeRepository cityRegionRepo = mock(CityRegionCodeRepository.class);
+        when(cityRegionRepo.findAll()).thenReturn(cityRegions);
+
+        ShortGridClientImpl client = new ShortGridClientImpl(null, null, null, null, cityRegionRepo);
+
+        // when
+        List<Coordinates> actual = ReflectionTestUtils.invokeMethod(client, "getCoords");
+
+        // then
+        assertThat(actual).hasSize(2);
     }
 }
