56db2b8198c196ef4e543c5716547d8201b7bd89
Yun531 <jaeyoongh315@gmail.com>
2026-04-27 11:42:50 +0900

    chore(warning): IssuedWarningLoadService 및 contract 패키지 삭제

--- src/main/java/com/github/yun531/climate/warning/contract/IssuedWarningReader.java
+++
@@ -1,7 +0,0 @@
-package com.github.yun531.climate.warning.contract;
-
-import java.util.List;
-
-public interface IssuedWarningReader {
-    List<IssuedWarning> loadIssuedWarnings(String regionId);
-}
