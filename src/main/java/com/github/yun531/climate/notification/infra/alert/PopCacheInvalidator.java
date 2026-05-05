ab525a0184646802166977a498573062c781289d
Yun531 <jaeyoongh315@gmail.com>
2026-05-05 11:24:39 +0900

    feat: 캐시 무효화/회전 INFO 로깅

--- src/main/java/com/github/yun531/climate/notification/infra/alert/PopCacheInvalidator.java
+++ src/main/java/com/github/yun531/climate/notification/infra/alert/PopCacheInvalidator.java
@@ -4,6 +4,7 @@ import com.github.yun531.climate.common.event.MidCollectionRefreshedEvent;
 import com.github.yun531.climate.common.event.ShortGridRefreshedEvent;
 import com.github.yun531.climate.common.event.ShortLandRefreshedEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -12,6 +13,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
  * 수집 완료 이벤트 수신 → POP 캐시 rotate.
  * - rotate: 기존 current → previous로 이동, current 비움.
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class PopCacheInvalidator {
@@ -21,15 +23,18 @@ public class PopCacheInvalidator {
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     void on(ShortGridRefreshedEvent e) {
         cacheManager.rotate();
+        log.info("POP 캐시 회전. event=ShortGridRefreshedEvent");
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     void on(ShortLandRefreshedEvent e) {
         cacheManager.rotate();
+        log.info("POP 캐시 회전. event=ShortLandRefreshedEvent");
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     void on(MidCollectionRefreshedEvent e) {
         cacheManager.rotate();
+        log.info("POP 캐시 회전. event=MidCollectionRefreshedEvent");
     }
 }
